907aad08d0ce50118b87057bf52f17d6de9b716c
Ionică Bizău <bizauionica@gmail.com>
2016-07-06 14:58:08 +0300

    Add example
    ...and a list with cool stuff by @skidding, @victorstanciu, @flaviusmatis, @cthackers, @ghinda, @alessioalex and @balajmarius 😎

--- example/index.js
+++ example/index.js
@@ -2,4 +2,23 @@
 
 const madeIn = require("../lib");
 
-console.log(madeIn());
+// Some cool JS stuff made in Romania
+madeIn("Romania", {
+    language: "JavaScript"
+  , token: "..."
+}, (err, data) => {
+    console.log(err || data.map(c => c.full_name).join("\n"));
+    // IonicaBizau/git-stats
+    // IonicaBizau/scrape-it
+    // skidding/cosmos
+    // victorstanciu/dbv
+    // skidding/dragdealer
+    // flaviusmatis/simplePagination.js
+    // cthackers/adm-zip
+    // ghinda/jotted
+    // alessioalex/ClientManager
+    // IonicaBizau/image-to-ascii
+    // IonicaBizau/medium-editor-markdown
+    // balajmarius/adi.js
+    // ...
+});
